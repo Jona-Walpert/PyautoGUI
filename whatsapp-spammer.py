d68f76449b14f8d169763bddfa006f8f0ef1e6fb
Jona Walpert <98217482+Jona-Walpert@users.noreply.github.com>
2022-01-22 21:30:54 +0100

    Add files via upload

---
+++ whatsapp-spammer.py
@@ -0,0 +1,9 @@
+import pyautogui as pg
+import time
+
+if pyautogui.hotkey('ctrl', 'shift', 's'):
+	while True:
+    	pg.write("..")
+    	pg.press("enter")
+    	if pyautogui.hotkey('ctrl', 'shift', 'e')
+    		break
