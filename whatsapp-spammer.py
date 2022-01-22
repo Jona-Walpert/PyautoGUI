6620178b93533e23827e657e7cf3bace7dce7d0a
Jona Walpert <98217482+Jona-Walpert@users.noreply.github.com>
2022-01-22 21:31:57 +0100

    Update whatsapp-spammer.py

--- whatsapp-spammer.py
+++ whatsapp-spammer.py
@@ -1,9 +1,10 @@
 import pyautogui as pg
 import time
 
-if pyautogui.hotkey('ctrl', 'shift', 's'):
+if pyautogui.hotkey('ctrl', 'shift', 's'): #for starting the spammer
 	while True:
     	pg.write("..")
     	pg.press("enter")
-    	if pyautogui.hotkey('ctrl', 'shift', 'e')
+    	if pyautogui.hotkey('ctrl', 'shift', 'e'): #for ending the spammer
     		break
+#if the hotkeys won't work. Please contact me!
